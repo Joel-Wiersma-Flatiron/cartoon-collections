c23a4c33921e8a0b8abc0fcd207a4e9a0d619ddd
Joel-Wiersma-Flatiron <Joel.Wiersma.D@gmail.com>
2020-10-29 20:03:56 -0600

    commit tree e8d3ae11b39b6b2774c3f081407e4f46a6e23268

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,29 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  count = 1
+  dwarves.each do |dwarf|
+    puts "#{count}. #{dwarf}"
+    count += 1
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(elements)
+  array = []
+  elements.each {|element| array << "#{element.capitalize}!"}
+  array
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  check = false
+  calls.each {|call| check = true if call.length > 4}
+  return check
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(elements)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  elements.find do |element|
+    cheese_types.find do |cheese|
+      element.eql?("#{cheese}")
+    end
+  end
 end
